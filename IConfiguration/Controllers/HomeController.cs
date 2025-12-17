cab17fd9a78d0a7cec4430bcec4ec89a118f2a5c
vlad-cv <me@vladcv.dev>
2025-12-17 03:07:01 -0600

    Rename project to IConfigurationExample and enhance config usage
    Renamed project and updated solution to use IConfigurationExample. Refactored HomeController to use dependency injection for IConfiguration and display weatherapi ClientId and ClientSecret in the view. Improved /home endpoint to show configuration values. Updated appsettings.json with real GUIDs for weatherapi settings. Adjusted Index.cshtml to display configuration values.

--- IConfiguration/Controllers/HomeController.cs
+++ IConfiguration/Controllers/HomeController.cs
@@ -1,14 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+ 
 
-namespace IConfiguration.Controllers
+namespace IConfigurationExample.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IConfiguration _configuration; 
+        public HomeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         [Route("/")]
         public IActionResult Index()
         {
+            IConfigurationSection weatherapi = _configuration.GetSection("weatherapi");
 
+
+            ViewBag.ClientId = weatherapi.GetValue<Guid>("ClientId");
+            ViewBag.ClientSecret = weatherapi.GetValue<Guid>("ClientSecret");
             return View();
         }
     }
+
+     
 }
--- IConfiguration/Program.cs
+++ IConfiguration/Program.cs
@@ -1,12 +1,18 @@
-using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
-app.MapGet("/home", async (httpContext) => {
+app.MapGet("/home", async (httpContext) =>
+{
     string? clientId = app.Configuration["weatherapi:ClientId"];
-    await httpContext.Response.WriteAsync($"Weather API Client ID: {clientId}");
+    await httpContext.Response.WriteAsync($"Weather API Client Id: {clientId}\n");
+
+    await httpContext.Response.WriteAsync($"Weather API Client Secret: {app.Configuration.GetValue<Guid>("weatherapi:ClientSecret").ToString()}\n");
+
+    await httpContext.Response.WriteAsync($"Weather API Client Id: {app.Configuration.GetSection("weatherapi").GetValue<string>("ClientId", "Default value")}\n");
+
 });
 
 app.MapDefaultControllerRoute();
